0faceae37504ce8e720ec3feee85198ea2f67182
angelo <aperezr@unsa.edu.pe>
2024-04-16 12:52:56 -0500

    Se creo la ruta para get usuarios, se creo middlewares de autorización de token y se implemento esos middlewre en la ruta get usuarios

--- src/rutas/RutaUsuarios.js
+++ src/rutas/RutaUsuarios.js
@@ -1,11 +1,16 @@
 const ControladorUsuario = require("../controladores/ControladorUsuario");
-// const Auth = require("../utils/AuthMiddlewares");
+const Auth = require("../utilidades/AuthMiddlewares");
 const RutaUsuarios = (base, app) => {
   const controlador = new ControladorUsuario();
   app.post(`${base}/crear-admin`, async (req, res, next) => {
     try {
-      const { nombre,email, clave,apellido } = req.body;
-      const resp = await controlador.CrearNuevoAdmin(nombre,apellido,email, clave);
+      const { nombre, email, clave, apellido } = req.body;
+      const resp = await controlador.CrearNuevoAdmin(
+        nombre,
+        apellido,
+        email,
+        clave
+      );
       return res.status(201).json(resp);
     } catch (error) {
       console.error("Error al crear un nuevo usuario -->", error);
@@ -15,13 +20,29 @@ const RutaUsuarios = (base, app) => {
         .json({ mesage: "Ocurrio un error al intentar crear usuario" });
     }
   });
-  app.post(`${base}/login`,async(req,res,next)=>{
+  app.post(`${base}/login`, async (req, res, next) => {
     try {
-      const resp = await controlador.Login(req,res)
-      return resp
+      const resp = await controlador.Login(req, res);
+      return resp;
     } catch (error) {
-      next(error)
+      next(error);
     }
-  })
+  });
+  app.get(`${base}/`,Auth.esAutorizado,Auth.esAdmin, async (req, res, next) => {
+    try {
+      // const resp = await controlador.Login(req,res)
+      // return resp
+
+      console.log(req.query);
+      console.log(req.usuario);
+      return res
+        .status(200)
+        .json({ message: "Hiciste get a todos los usuarios" });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ mesage: "Ocurrio un error al intentar crear usuario" });
+    }
+  });
 };
 module.exports = RutaUsuarios;
