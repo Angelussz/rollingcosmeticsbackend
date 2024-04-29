02feb2d7b1ee5ece98fe4c8037b3a02a1bd61710
Facundo <facupalacios21@gmail.com>
2024-04-28 21:31:22 -0300

    se cambio el validador de clave

--- src/utilidades/validadores.js
+++ src/utilidades/validadores.js
@@ -1,5 +1,5 @@
 const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-const regexPassword = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/
+const regexPassword = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!*@#$%^&+=])(?=.{6,20})/
 
 function ValidarEmail(evalEmail){
     return regexEmail.test(evalEmail)
